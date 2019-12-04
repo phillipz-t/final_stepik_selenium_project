76adf45aa839a24c8c784f1a71ac21b45fe6941b
phillipz-t <llllllllllllllllllll20lllll5@gmail.com>
2019-12-04 14:55:48 +0100

    Optimized code with adaptive classes

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,13 @@
+from .pages.login_page import LoginPage
 from .pages.main_page import MainPage
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)
     page.open()
-    page.should_be_login_link()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
+	
+
+
+
